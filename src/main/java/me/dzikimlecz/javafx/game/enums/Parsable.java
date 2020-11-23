49bef2919013fff98cf09c1deeed3ff6c82b3662
dzikimlecz <j.pul312@gmail.com>
2020-11-23 20:51:58 +0100

    Modified enums parsing
    Old:
    1st char Uppercase rest lower, "_" swapped with " "
    New:
    1st char OF EVERY WORD Uppercase rest lower, "_" swapped with " "

---
+++ src/main/java/me/dzikimlecz/javafx/game/enums/Parsable.java
@@ -0,0 +1,23 @@
+package me.dzikimlecz.javafx.game.enums;
+
+/**
+ * Interface used to parse an enum value to readable string.
+ */
+public interface Parsable {
+	String name();
+	default String parseString() {
+		String name = name();
+		StringBuilder nameBuilder = new StringBuilder();
+		
+		while (name.contains("_")) {
+			nameBuilder.append(name.charAt(0))
+					.append(name.toLowerCase(), 1, name.indexOf('_'))
+					.append(' ');
+			name = name.substring(name.indexOf('_') + 1);
+		}
+		nameBuilder.append(name.charAt(0))
+				.append(name.toLowerCase(), 1, name.length());
+		
+		return nameBuilder.toString();
+	}
+}
